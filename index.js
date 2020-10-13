215902fe6989a63469e554361f8fa8c78d8932ee
Music <musicm987@gmail.com>
2020-10-13 09:53:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// const arrayOfNames = ["Lisa", "Kaitlin", "Jan"]
+function writeCards(arrayOfNames, eventName) {
+    const newArray = [];
+    for(let i = 0; i < arrayOfNames.length; i++) {
+        newArray.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+}
+
+// let countDown = 10;
+// while (countDown >= 0) {
+//     console.log(countDown--)
+// }
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
